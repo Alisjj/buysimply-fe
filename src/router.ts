46706bcbdff8aeb41c0e1b48aecd8a582688957f
Aliyu Sani <aliyu.alsajjadsani@vegaitglobal.com>
2026-03-18 17:02:05 +0100

    fixL: routes

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from './views/HomeView.vue'
 import LoginView from './views/LoginView.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
